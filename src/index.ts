2768acabeff78a0fdf4d5a7c93d132889b3b77b1
Berkay Akçay <berkayakcay.m@gmail.com>
2024-09-27 07:59:10 +0300

    z export

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { loadConfig } from "./parser";
 export { envalidation } from "./envalidation";
 export type { TypeOf } from "./envalidation";
 import * as zod from "zod";
+
+export * from "zod";
